2844c5168504b012619a73097ef486b89c057720
shiva <shivamaharjan1234567890@gmail.com>
2019-04-01 11:33:54 +0545

    finished loading pictures

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,15 +23,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rboHulk = findViewById(R.id.rboHulk);
         ivImage = findViewById(R.id.ivImage);
 
-
+        rboHulk.setOnClickListener(this);
+        rboBatman.setOnClickListener(this);
+        rboJoker.setOnClickListener(this);
 
     }
 
 
-
-
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rboBatman:
+                ivImage.setImageResource(R.drawable.batman);
+                Toast.makeText(this, "I'm Batman.", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.rboJoker:
+                ivImage.setImageResource(R.drawable.joker);
+                Toast.makeText(this, "Hello, I'm Joker.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rboHulk:
+                ivImage.setImageResource(R.drawable.hulk);
+                Toast.makeText(this, "You won't like me GREEN.", Toast.LENGTH_LONG).show();
+                break;
+        }
 
     }
 }
