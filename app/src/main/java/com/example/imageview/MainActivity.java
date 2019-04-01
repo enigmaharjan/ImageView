8163169d5aac67cdeabf7705bc25d420148e940c
shiva <shivamaharjan1234567890@gmail.com>
2019-04-01 10:43:06 +0545

    added images in drawable folder

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.imageview;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RadioButton rboHulk, rboBatman, rboJoker;
+    private ImageView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rboBatman = findViewById(R.id.rboBatman);
+        rboJoker = findViewById(R.id.rboJoker);
+        rboHulk = findViewById(R.id.rboHulk);
+        ivImage = findViewById(R.id.ivImage);
+
+
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
